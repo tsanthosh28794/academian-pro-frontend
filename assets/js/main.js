da1960148df8eb7dddab99715c88a6a34eef675c
tsanthosh28794 <130087785+tsanthosh28794@users.noreply.github.com>
2026-03-03 13:26:49 +0530

    feat: Introduce modular CSS architecture and responsive navigation with a mega-menu and overlay.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Project Loaded Successfully 🚀");
-});
+
+const navToggle = document.getElementById("navToggle");
+const navMenu = document.getElementById("navMenu");
+const overlay = document.getElementById("overlay");
+
+if (navToggle && navMenu && overlay) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+
+  overlay.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+}
